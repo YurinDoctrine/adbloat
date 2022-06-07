71c97cbaaf6869c9c2e7ba9e677eb179ea3161a5
Yurin Doctrine <43861005+YurinDoctrine@users.noreply.github.com>
2022-06-07 21:38:13 +0300

    edit adbloat.cmd

--- adbloat.cmd
+++ adbloat.cmd
@@ -1384,6 +1384,7 @@ echo Optimizing ...
     adb shell settings put global cached_apps_freezer enabled
     adb shell settings put system tube_amp_effect 1
     adb shell settings put system k2hd_effect 1
+    adb shell settings put system screen_auto_brightness_adj 0
 echo ALL DONE!
 echo.
 adb kill-server
