699d4535f528e9b70c5708bdf9dfe723d07ff1cc
Yurin Doctrine <43861005+YurinDoctrine@users.noreply.github.com>
2022-06-09 22:05:43 +0300

    edit adbloat.cmd

--- adbloat.cmd
+++ adbloat.cmd
@@ -1388,6 +1388,7 @@ echo Optimizing ...
     adb shell settings put system k2hd_effect 1
     adb shell settings put system screen_auto_brightness_adj 0
     adb shell settings put system font_scale 1.25
+    adb shell settings put global app_standby_enabled 1
 echo ALL DONE!
 echo.
 adb kill-server
