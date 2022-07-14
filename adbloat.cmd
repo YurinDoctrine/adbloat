00be49e90d1a78db9d30b5a2ca054615212799c9
Yurin Doctrine <doctriney@protonmail.com>
2022-07-14 09:17:36 +0300

    edit adbloat.cmd

--- adbloat.cmd
+++ adbloat.cmd
@@ -1410,7 +1410,8 @@ echo Optimizing ...
     adb shell settings put global app_auto_restriction_enabled 1
     adb shell settings put global app_standby_enabled 1
     adb shell settings put global forced_app_standby_enabled 1
-    adb shell settings put global keep_profile_in_background 0 
+    adb shell settings put global keep_profile_in_background 0
+    adb shell settings put global adaptive_battery_management_enabled 1
     adb shell settings put system POWER_SAVE_PRE_CLEAN_MEMORY_TIME 1800
     adb shell settings put system tube_amp_effect 1
     adb shell settings put system k2hd_effect 1
