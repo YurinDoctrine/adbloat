aa32d3cdfd2aef13eae2d98fea243afbc62952d6
Yurin Doctrine <doctriney@protonmail.com>
2022-09-23 23:26:13 +0300

    edit adbloat.sh

--- adbloat.sh
+++ adbloat.sh
@@ -1458,6 +1458,7 @@ start() {
     adb shell settings put global debug.egl.profiler 1
     adb shell settings put global debug.overlayui.enable 1
     adb shell settings put global dev.bootcomplete 0
+    adb shell settings put global ro.warmboot.capability 1
     adb shell settings put global ro.secure 0
     adb shell settings put global ro.config.hw_quickpoweron true
     adb shell settings put global ro.config.hw_power_saving true
