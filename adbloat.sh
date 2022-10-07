e36df3ba033d2d330fd7480033c53b75d0b36ab7
Yurin Doctrine <doctriney@protonmail.com>
2022-10-07 13:16:27 +0300

    edit adbloat.sh

--- adbloat.sh
+++ adbloat.sh
@@ -1490,6 +1490,7 @@ start() {
     adb shell settings put global hw3d.force 1
     adb shell settings put global debug.enabletr true
     adb shell settings put global debug.sf.hw 1
+    adb shell settings put global debug.sf.enable_hwc_vds 1
     adb shell settings put global debug.egl.hw 1
     adb shell settings put global debug.egl.profiler 1
     adb shell settings put global debug.overlayui.enable 1
