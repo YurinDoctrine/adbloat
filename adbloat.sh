2420d1738deec534af7242f58dd648da9c26278d
Yurin Doctrine <doctriney@protonmail.com>
2022-10-18 21:32:17 +0300

    edit adbloat.sh

--- adbloat.sh
+++ adbloat.sh
@@ -1425,6 +1425,7 @@ start() {
     adb shell settings put global dalvik.vm.verify-bytecode false
     adb shell settings put global dalvik.vm.usejit true
     adb shell settings put global dalvik.vm.checkjni false
+    adb shell settings put global dalvik.vm.check-dex-sum false
     adb shell settings put global dalvik.vm.dexopt-flags m=y,v=n,o=y,u=n
     adb shell settings put global ro.config.hw_fast_dormancy 0,0
     adb shell settings put secure upload_debug_log_pref 0
