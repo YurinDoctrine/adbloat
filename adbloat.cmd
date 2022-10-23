d287f6a909712655092da1bb179014abcb355067
Yurin Doctrine <doctriney@protonmail.com>
2022-10-23 13:19:27 +0300

    edit adbloat.cmd

--- adbloat.cmd
+++ adbloat.cmd
@@ -1436,6 +1436,7 @@ echo Optimizing ...
     adb shell settings put global dalvik.vm.check-dex-sum false
     adb shell settings put global dalvik.vm.debug.alloc 0
     adb shell settings put global dalvik.vm.dexopt-flags m=y,v=n,o=y,u=n
+    adb shell settings put global dalvik.vm.execution-mode int:jit
     adb shell settings put global ro.config.hw_fast_dormancy 0,0
     adb shell settings put secure upload_debug_log_pref 0
     adb shell settings put secure upload_log_pref 0
