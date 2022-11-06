59d56cdff68ef6e0e70d86b16262fbd175153be0
Yurin Doctrine <doctriney@protonmail.com>
2022-11-06 16:54:26 +0300

    edit adbloat.cmd

--- adbloat.cmd
+++ adbloat.cmd
@@ -1527,9 +1527,9 @@ echo Optimizing ...
     adb shell settings put global app_auto_restriction_enabled 1
     adb shell settings put global app_standby_enabled 1
     adb shell settings put global forced_app_standby_enabled 1
-    adb shell settings put global ro.sys.fw.bservice_enable true
     adb shell settings put global keep_profile_in_background 0
     adb shell settings put global adaptive_battery_management_enabled 1
+    adb shell settings put global ro.sys.fw.bservice_enable true
     adb shell settings put global fstrim_mandatory_interval 1
     adb shell settings put global ro.sys.fw.use_trim_settings true
     adb shell settings put global ro.trim.config true
