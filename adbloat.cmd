ac2d04a3d2da0c4d4e53b7c1e8c64a4c04a8ec7c
Yurin Doctrine <doctriney@protonmail.com>
2022-11-28 18:10:36 +0300

    edit adbloat.cmd

--- adbloat.cmd
+++ adbloat.cmd
@@ -1529,6 +1529,7 @@ echo Optimizing ...
     adb shell settings put global ro.zygote.preload.disable 2
     adb shell settings put global ro.zygote.disable_gl_preload false
     adb shell settings put global persist.device_config.runtime_native.usap_pool_enabled true
+    adb shell settings put global persist.device_config.runtime_native_boot.iorap_readahead_enable true
     adb shell settings put global ro.service.remove_unused 1
     adb shell settings put global ro.mtk_perfservice_support 0
     adb shell settings put global debug.sqlite.syncmode 1
