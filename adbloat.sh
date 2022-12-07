4fc798205c5ebc6420d1d92adcba4c25f230d244
Yurin Doctrine <doctriney@protonmail.com>
2022-12-07 10:53:26 +0300

    edit adbloat.sh

--- adbloat.sh
+++ adbloat.sh
@@ -1432,6 +1432,7 @@ start() {
     adb shell settings put global persist.sys.dalvik.hyperthreading true
     adb shell settings put global dalvik.vm.heaputilization 0.25
     adb shell settings put global dalvik.vm.heaptargetutilization 0.25
+    adb shell settings put global dalvik.vm.dex2oat64.enabled true
     adb shell settings put global dalvik.vm.dex2oat-swap true
     adb shell settings put global dalvik.vm.verify-bytecode false
     adb shell settings put global dalvik.vm.usejit true
