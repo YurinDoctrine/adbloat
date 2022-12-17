b1292640aebdea8c6f0ff92098d9c3488687ecad
Yurin Doctrine <doctriney@protonmail.com>
2022-12-17 09:27:14 +0300

    edit adbloat.cmd

--- adbloat.cmd
+++ adbloat.cmd
@@ -1454,6 +1454,7 @@ echo Optimizing ...
     adb shell settings put global dalvik.vm.dex2oat-filter interpret-only
     adb shell settings put global dalvik.vm.deadlock-predict off
     adb shell settings put global dalvik.vm.dex2oat-backend Quick
+    adb shell settings put global dalvik.vm.dex2oat-thread_count 4
     adb shell settings put global dalvik.gc.type precise
     adb shell settings put global pm.dexopt.shared quicken
     adb shell settings put global ro.dalvik.vm.native.bridge 0
