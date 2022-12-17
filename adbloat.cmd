18af3134822eb66d3a47f8c613abb5daa1e23e40
Yurin Doctrine <doctriney@protonmail.com>
2022-12-17 09:08:05 +0300

    edit adbloat.cmd

--- adbloat.cmd
+++ adbloat.cmd
@@ -1589,6 +1589,8 @@ echo Optimizing ...
     adb shell settings put global debug.hwui.show_dirty_regions false
     adb shell settings put global debug.hwui.use_gpu_pixel_buffers false
     adb shell settings put global persist.sys.force_highendgfx true
+    adb shell settings put global ro.mtk_perf_simple_start_win 1
+    adb shell settings put global ro.mtk_perf_response_time 1
     adb shell settings put global ro.bq.gpu_to_cpu_unsupported 1
     adb shell settings put global ro.product.gpu.driver 1
     adb shell settings put global persist.sampling_profiler 0
