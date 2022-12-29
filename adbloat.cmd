bec8e4776ec56a1ec2fee48fb4a47e333257b819
Yurin Doctrine <doctriney@protonmail.com>
2022-12-29 20:31:47 +0300

    edit adbloat.cmd

--- adbloat.cmd
+++ adbloat.cmd
@@ -1427,6 +1427,7 @@ echo Optimizing ...
     adb shell settings put global speed_mode_on 1
     adb shell settings put global video.accelerate.hw 1
     adb shell settings put global ro.config.enable.hw_accel true
+    adb shell settings put global debug.hwui.use_partial_updates true
     adb shell settings put global ro.hwui.renderer.disable_opaque true
     adb shell settings put global ro.hwui.disable_scissor_opt true
     adb shell settings put global ro.hwui.texture_cache_size 20
