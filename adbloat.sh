5723455c841100703d0f266b5d361a60d789e9d9
Yurin Doctrine <doctriney@protonmail.com>
2022-12-29 20:44:26 +0300

    edit adbloat.sh

--- adbloat.sh
+++ adbloat.sh
@@ -1575,6 +1575,8 @@ start() {
     adb shell settings put global ro.config.low_ram true
     adb shell settings put global ro.config.low_ram.mod true
     adb shell settings put global ro.board_ram_size low
+    adb shell settings put global ro.ime.lowmemory true
+    adb shell settings put global ro.cp_system_other_odex 1
     adb shell settings put global ro.config.dha_tunnable 1
     adb shell settings put global ro.cpufreq.game 1
     adb shell settings put global ro.core_ctl_min_cpu 0
