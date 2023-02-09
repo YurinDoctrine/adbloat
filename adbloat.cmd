1015d4a68622fe7b03cc649d4c7e26c0a0dce8fb
Yurin Doctrine <doctriney@protonmail.com>
2023-02-09 19:31:10 +0300

    edit adbloat.cmd

--- adbloat.cmd
+++ adbloat.cmd
@@ -1436,6 +1436,7 @@ echo Optimizing ...
     adb shell settings put global ro.hwui.drop_shadow_cache_size 1
     adb shell settings put global ro.hwui.shape_cache_size 1
     adb shell settings put global ro.hwui.r_buffer_cache_size 1
+    adb shell settings put global ro.hwui.path_cache_size 1
     adb shell settings put global omap.enhancement true
     adb shell settings put global ENFORCE_PROCESS_LIMIT false
     adb shell settings put global enhanced_processing 1
