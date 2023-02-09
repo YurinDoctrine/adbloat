3fd8149342b03a4ca2a4d4024d0ca0d87e8d21c2
Yurin Doctrine <doctriney@protonmail.com>
2023-02-09 19:22:54 +0300

    edit adbloat.sh

--- adbloat.sh
+++ adbloat.sh
@@ -1426,6 +1426,9 @@ start() {
     adb shell settings put global ro.hwui.texture_cache_size 20
     adb shell settings put global ro.hwui.texture_cache_flush_rate 0.5
     adb shell settings put global ro.hwui.gradient_cache_size 0.1
+    adb shell settings put global ro.hwui.drop_shadow_cache_size 1
+    adb shell settings put global ro.hwui.shape_cache_size 1
+    adb shell settings put global ro.hwui.r_buffer_cache_size 1
     adb shell settings put global omap.enhancement true
     adb shell settings put global ENFORCE_PROCESS_LIMIT false
     adb shell settings put global enhanced_processing 1
