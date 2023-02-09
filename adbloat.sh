f93f3fa74dddcda118b16170aa2a58ae4cc0fdab
Yurin Doctrine <doctriney@protonmail.com>
2023-02-09 18:24:56 +0300

    edit adbloat.sh

--- adbloat.sh
+++ adbloat.sh
@@ -1530,7 +1530,7 @@ start() {
     adb shell settings put global audio.deep_buffer.media true
     adb shell settings put global audio.offload.video true
     adb shell settings put global audio.offload.gapless.enabled true
-    adb shell settings put global audio.offload.pcm.16bit.enable false
+    adb shell settings put global audio.offload.pcm.24bit.enable false
     adb shell settings put global tunnel.decode false
     adb shell settings put global persist.speaker.prot.enable false
     adb shell settings put global persist.audio.hifi true
