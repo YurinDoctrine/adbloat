703a4a0e4d17f0870712c9ac62d5fef6f6321fce
Yurin Doctrine <doctriney@protonmail.com>
2023-02-11 10:25:33 +0300

    edit adbloat.cmd

--- adbloat.cmd
+++ adbloat.cmd
@@ -1678,6 +1678,7 @@ echo Optimizing ...
     adb shell settings put global APP_SWITCH_DELAY_TIME false
     adb shell settings put global ACTIVITY_INACTIVITY_RESET_TIME false
     adb shell settings put global sys.autobrightness_optimize true
+    adb shell settings put global persist.perf.wm_static_blur true
     adb shell settings put system intelligent_sleep_mode 0
     adb shell settings put system POWER_SAVE_PRE_CLEAN_MEMORY_TIME 1800
     adb shell settings put system tube_amp_effect 1
