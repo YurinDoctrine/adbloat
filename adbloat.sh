b1f5332c85afd2add1f1b1cc60cce1b5135fe8ba
Yurin Doctrine <doctriney@protonmail.com>
2023-02-18 09:37:14 +0300

    edit adbloat.sh

--- adbloat.sh
+++ adbloat.sh
@@ -1468,6 +1468,7 @@ start() {
     adb shell settings put global dalvik.gc.type precise
     adb shell settings put global pm.dexopt.shared quicken
     adb shell settings put global ro.dalvik.vm.native.bridge 0
+    adb shell settings put global vnswap.enabled false
     adb shell settings put global ro.config.hw_fast_dormancy 0,0
     adb shell settings put global ro.config.hw_dts true
     adb shell settings put secure upload_debug_log_pref 0
