8e504465839864699a16a01d620bd1a8a1183a3b
Yurin Doctrine <doctriney@protonmail.com>
2023-05-12 23:21:19 +0300

    edit adbloat.cmd

--- adbloat.cmd
+++ adbloat.cmd
@@ -1720,7 +1720,7 @@ echo Optimizing ...
     adb shell settings put global dalvik.gc.type precise
     adb shell settings put global pm.dexopt.shared quicken
     adb shell settings put global pm.dexopt.boot verify
-    adb shell settings put global pm.dexopt.first-boot verify
+    adb shell settings put global pm.dexopt.first-boot quicken
     adb shell settings put global ro.dalvik.vm.native.bridge 0
     adb shell settings put global vnswap.enabled false
     adb shell settings put global cgroup_disable memory
