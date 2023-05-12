59e17432a0b08a2c845d2b41bfdab803be56d887
Yurin Doctrine <doctriney@protonmail.com>
2023-05-12 23:22:40 +0300

    edit adbloat.cmd

--- adbloat.cmd
+++ adbloat.cmd
@@ -1721,6 +1721,7 @@ echo Optimizing ...
     adb shell settings put global pm.dexopt.shared quicken
     adb shell settings put global pm.dexopt.boot verify
     adb shell settings put global pm.dexopt.first-boot quicken
+    adb shell settings put global pm.dexopt.install quicken
     adb shell settings put global ro.dalvik.vm.native.bridge 0
     adb shell settings put global vnswap.enabled false
     adb shell settings put global cgroup_disable memory
