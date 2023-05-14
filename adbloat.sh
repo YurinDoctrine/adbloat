2a4f621a3ce797c70c1dc41b8f0c5b10a5d144cf
Yurin Doctrine <doctriney@protonmail.com>
2023-05-14 23:14:03 +0300

    edit adbloat.sh

--- adbloat.sh
+++ adbloat.sh
@@ -1901,6 +1901,7 @@ start() {
     adb shell settings put global persist.device_config.runtime_native_boot.iorap_perfetto_enable true
     adb shell settings put global ro.service.remove_unused 1
     adb shell settings put global ro.mtk_perfservice_support 0
+    adb shell settings put global vendor.mtk_thumbnail_optimization true
     adb shell settings put global debug.sqlite.journalmode WAL
     adb shell settings put global debug.sqlite.syncmode 1
     adb shell settings put global ro.incremental.enable 1
