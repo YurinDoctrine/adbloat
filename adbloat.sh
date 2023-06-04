3be5dd78b757881c56ece1a322fddd907ee5695b
Yurin Doctrine <doctriney@protonmail.com>
2023-06-04 17:00:46 +0300

    edit adbloat.sh

--- adbloat.sh
+++ adbloat.sh
@@ -1622,7 +1622,7 @@ start() {
     #adb shell wm density 390
 
     adb shell settings put global ro.fps_enable 1
-    adb shell settings put global ro.fps.capsmin 60fps
+    adb shell settings put global ro.fps.capsmin 60
     adb shell settings put global dont.lower.fps true
     adb shell settings put global stabilizer.fps true
     adb shell settings put global stable.fps.enable true
