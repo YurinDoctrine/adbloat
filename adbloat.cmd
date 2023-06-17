521310daf7c25162faa7aef49cf5aa8746150df3
Yurin Doctrine <doctriney@protonmail.com>
2023-06-17 22:34:49 +0300

    edit adbloat.cmd

--- adbloat.cmd
+++ adbloat.cmd
@@ -79,6 +79,8 @@ exit 0
     adb shell rm -rf /storage/sdcard1/LOST.DIR/*
     adb shell sm fstrim
 
+    adb shell setprop persist.log.tag V
+
     ::adb shell wm size 1080x1920
     ::adb shell wm density 390
 
