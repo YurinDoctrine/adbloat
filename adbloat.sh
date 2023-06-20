1f0435f8c2ddfe18ec51426c1188eae76b842027
Yurin Doctrine <doctriney@protonmail.com>
2023-06-20 19:19:50 +0300

    edit adbloat.sh

--- adbloat.sh
+++ adbloat.sh
@@ -383,6 +383,8 @@ tweaks() {
     adb shell settings put global media.stagefright.enable-meta true
     adb shell settings put global media.stagefright.enable-http true
     adb shell settings put global persist.media.hls.enhancements true
+    adb shell settings put global aaudio.mmap_policy 1
+    adb shell settings put global aaudio.mmap_exclusive_policy 2
     adb shell settings put global audio.deep_buffer.media true
     adb shell settings put global audio.offload.disable true
     adb shell settings put global audio.offload.video true
