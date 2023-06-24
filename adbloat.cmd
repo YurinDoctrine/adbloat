fb4b71ddf07608388bfc91bf5ae67c8c8dcf6981
Yurin Doctrine <doctriney@protonmail.com>
2023-06-24 23:48:48 +0300

    edit adbloat.cmd

--- adbloat.cmd
+++ adbloat.cmd
@@ -364,6 +364,7 @@ exit 0
     adb shell settings put global ro.allow.mock.location 1
     adb shell settings put global ro.com.google.locationfeatures 0
     adb shell settings put global ro.com.google.networklocation 0
+    adb shell settings put global ro.ril.def.agps.feature 0
     adb shell settings put global ro.ril.def.agps.mode 0
     adb shell settings put global net.tethering.noprovisioning true
     adb shell settings put global security.perf_harden 1
