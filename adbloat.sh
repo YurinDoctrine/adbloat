968bba71f8aae90bac73b535f40c6e798fcc38c4
Yurin Doctrine <doctriney@protonmail.com>
2023-06-26 21:05:47 +0300

    edit adbloat.sh

--- adbloat.sh
+++ adbloat.sh
@@ -664,6 +664,7 @@ tweaks() {
     adb shell settings put global ro.qti.sensors.step_detector false
     adb shell settings put global ro.qti.sensors.pick_up false
     adb shell settings put global ro.vendor.sdk.sensors.gestures false
+    adb shell settings put global persist.radio.sar_sar_sensor false
     adb shell settings put global persist.metadata_dynfps.disable true
     adb shell settings put global persist.android.strictmode 0
     adb shell settings put global persist.sys.strictmode.disable 1
