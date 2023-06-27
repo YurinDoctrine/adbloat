e5860941b0ef18906b800966698bb6eaae7365b6
Yurin Doctrine <doctriney@protonmail.com>
2023-06-27 22:11:55 +0300

    edit adbloat.cmd

--- adbloat.cmd
+++ adbloat.cmd
@@ -734,6 +734,10 @@ exit 0
     adb shell settings put secure screensaver_activate_on_sleep 0
     adb shell settings put secure screensaver_activate_on_dock 0
     adb shell settings put secure wake_gesture_enabled 0
+    adb shell settings put secure volume_hush_gesture 0
+    adb shell settings put secure hush_gesture_used 0
+    adb shell settings put secure silence_gesture 0
+    adb shell settings put secure skip_gesture 0
     adb shell settings put secure fingerprint_effect 0
     adb shell settings put secure adaptive_sleep 0
     adb shell settings put secure game_auto_temperature_control 0
