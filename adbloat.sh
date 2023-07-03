b651642fde6696dffb92aac5b295edf11c367aed
Yurin Doctrine <doctriney@protonmail.com>
2023-07-03 20:40:46 +0300

    edit adbloat.sh

--- adbloat.sh
+++ adbloat.sh
@@ -608,7 +608,7 @@ tweaks() {
     adb shell settings put global persist.smart_pool 1
     adb shell settings put global ro.hwui.render_ahead 1
     adb shell settings put global debug.hwui.renderer 1
-    adb shell settings put global debug.hwui.renderer_mode 0
+    adb shell settings put global debug.hwui.renderer_mode 1
     adb shell settings put global debug.hwui.level 0
     adb shell settings put global debug.hwui.swap_with_damage false
     adb shell settings put global debug.hwui.render_dirty_regions false
