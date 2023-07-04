5fd2efe7641b69f0a56aabefdc265354e757c1bd
Yurin Doctrine <doctriney@protonmail.com>
2023-07-04 11:39:02 +0300

    edit adbloat.sh

--- adbloat.sh
+++ adbloat.sh
@@ -403,6 +403,7 @@ tweaks() {
     adb shell settings put global ro.surface_flinger.enable_layer_caching true
     adb shell settings put global ro.surface_flinger.use_context_priority true
     adb shell settings put global ro.surface_flinger.start_graphics_allocator_service true
+    adb shell settings put global ro.surface_flinger.max_frame_buffer_acquired_buffers 1
     adb shell settings put global ro.surface_flinger.has_wide_color_display false
     adb shell settings put global persist.sys.sf.color_saturation 1.0
     adb shell settings put global persist.sys.sf.hs_mode 0
