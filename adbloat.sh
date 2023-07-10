a8f37a00f843225bd8f02947ed8a4ba6ade431d3
Yurin Doctrine <doctriney@protonmail.com>
2023-07-10 15:25:40 +0300

    edit adbloat.sh

--- adbloat.sh
+++ adbloat.sh
@@ -643,6 +643,7 @@ tweaks() {
     adb shell settings put global persist.sys.font_clarity 0
     adb shell settings put global persist.sys.force_highendgfx true
     adb shell settings put global ro.config.avoid_gfx_accel false
+    adb shell settings put global rs.gpu.renderscript 0
     adb shell settings put global force_gpu_rendering 1
     adb shell settings put global gpu_rendering_mode 1
     adb shell settings put global opengl_renderer 1
