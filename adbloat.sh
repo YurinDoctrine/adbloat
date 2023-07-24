c1c44cdddc9ec1a229544ce587f0df34cad23991
Yurin Doctrine <doctriney@protonmail.com>
2023-07-24 09:45:21 +0300

    edit adbloat.sh

--- adbloat.sh
+++ adbloat.sh
@@ -431,6 +431,8 @@ tweaks() {
     adb shell settings put global ro.surface_flinger.set_idle_timer_ms 250
     adb shell settings put global ro.surface_flinger.set_touch_timer_ms 500
     adb shell settings put global ro.surface_flinger.set_display_power_timer_ms 1000
+    adb shell settings put global ro.surface_flinger.vsync_event_phase_offset_ns 0
+    adb shell settings put global ro.surface_flinger.vsync_sf_event_phase_offset_ns 0
     adb shell settings put global ro.surface_flinger.use_content_detection_for_refresh_rate true
     adb shell settings put global ro.surface_flinger.refresh_rate_switching true
     adb shell settings put global ro.surface_flinger.enable_layer_caching true
