04708ee0a1db92134086a1b82a67485629c596ff
Yurin Doctrine <doctriney@protonmail.com>
2023-08-24 14:13:31 +0300

    edit adbloat.sh

--- adbloat.sh
+++ adbloat.sh
@@ -317,6 +317,7 @@ tweaks() {
     adb shell settings put global persist.sys.hw_statistics 0
     adb shell settings put global persist.sys.apps_statistics 0
     adb shell settings put global persist.sys.apr.enabled 0
+    adb shell settings put global persist.vendor.aprservice.enabled 0
     adb shell settings put global persist.vendor.verbose_logging_enabled false
     adb shell settings put global persist.debug.xlog.enable 0
     adb shell settings put global persist.meta.dumpdata 0
