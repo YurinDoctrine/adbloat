14f46f34220564d5a65cef74c6c26ecc6bdb3ef4
Yurin Doctrine <doctriney@protonmail.com>
2023-08-29 10:07:31 +0100

    edit adbloat.sh

--- adbloat.sh
+++ adbloat.sh
@@ -352,8 +352,8 @@ tweaks() {
     adb shell settings put secure adaptive_connectivity_enabled 0
     adb shell settings put global multipath-tcp-enable 1
     adb shell settings put global dns_resolvability_required 0
-    adb shell settings put global dns1 8.8.8.8
-    adb shell settings put global dns2 8.8.4.4
+    adb shell settings put global net.dns1 8.8.8.8
+    adb shell settings put global net.dns2 8.8.4.4
     adb shell settings put global private_dns_mode off
     adb shell settings put global wifi_mac_randomization 2
     adb shell settings put global wifi_connected_mac_randomization_supported 2
