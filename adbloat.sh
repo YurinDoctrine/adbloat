ed6c1e5374b801f0974b687e05f9cb6ac14bb1e4
Yurin Doctrine <doctriney@protonmail.com>
2023-09-11 14:42:55 +0300

    edit adbloat.sh

--- adbloat.sh
+++ adbloat.sh
@@ -179,9 +179,10 @@ tweaks() {
     adb shell settings put global ro.mds.enable true
     adb shell settings put global ro.ril.hep 1
     adb shell settings put global ro.mtk_lte_support 1
+    adb shell settings put global persist.radio.lte_enabled true
     adb shell settings put global ro.config.hw_lte_support true
     adb shell settings put global ro.config.hw_volte_on true
-    adb shell settings put global persist.radio.lte_enabled true
+    adb shell settings put global ro.config.hw_volte_dyn true
     adb shell settings put global persist.eons.enabled true
     adb shell settings put global persist.cust.tel.eons 1
     adb shell settings put global persist.sys.klo on
