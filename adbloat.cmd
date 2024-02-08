b2a7fd0b3528111dff75ff64b5e8a5dd59e3a284
Yurin Doctrine <doctriney@protonmail.com>
2024-02-08 18:32:13 +0300

    edit adbloat.cmd

--- adbloat.cmd
+++ adbloat.cmd
@@ -874,6 +874,7 @@ exit 0
     adb shell settings put global vold.should_defrag 1
     adb shell settings put global vold.checkpoint_committed 1
     adb shell settings put global ro.storaged.event.interval 999999
+    adb shell settings put global virtualsd.enable true
     adb shell settings put global gadget.nand.force_sync true
     adb shell settings put global pm.sdwake.enabled true
     adb shell settings put global ro.DontUseAnimate yes
