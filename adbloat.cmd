28bbb78db64f56c3e1395c0698a918456a2fda24
Yurin Doctrine <doctriney@protonmail.com>
2024-07-18 16:43:50 +0300

    edit adbloat.cmd

--- adbloat.cmd
+++ adbloat.cmd
@@ -1224,6 +1224,7 @@ goto :tweaks
     adb shell settings put secure adaptive_sleep 0
     adb shell settings put secure game_auto_temperature_control 0
     adb shell settings put secure sensors_off 1
+    adb shell settings put secure sensors_off_enabled 1
     adb shell settings put system background_power_saving_enable 1
     adb shell settings put system perf_profile performance
     adb shell settings put system intelligent_sleep_mode 0
