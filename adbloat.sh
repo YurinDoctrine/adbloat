d7c7281a42afd7a44ae34b088e8fb957e01faf6e
Yurin Doctrine <doctriney@protonmail.com>
2024-08-06 20:03:21 +0300

    edit adbloat.sh

--- adbloat.sh
+++ adbloat.sh
@@ -1255,8 +1255,8 @@ tweaks() {
     adb shell settings put system charging_info_always 0
     adb shell am broadcast -a android.intent.action.ACTION_OPTIMIZE_DEVICE
     adb shell am broadcast -a com.android.systemui.action.CLEAR_MEMORY
-    adb shell cmd activity kill-all
     adb shell am kill-all
+    adb shell cmd activity kill-all
     echo -e "ALL DONE!"
     echo -e ""
     adb kill-server
